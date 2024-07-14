130295f491a5d8ee3ef79e1ccbb19aa9aba5b70f
Marcelo A Coldibelli <marcelo.coldibelli@gmail.com>
2024-07-14 18:54:23 -0300

    feat: add user repository

---
+++ src/main/java/com/picpay/challenge_backend/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.picpay.challenge_backend.repositories;
+
+import com.picpay.challenge_backend.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findUserById(Long id);
+}
