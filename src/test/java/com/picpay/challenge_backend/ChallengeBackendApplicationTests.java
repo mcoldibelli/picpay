80c2c2ba401df578b9893a748d4cbf75f2a54bac
Marcelo A Coldibelli <marcelo.coldibelli@gmail.com>
2024-07-14 11:06:30 -0300

    feat: first commit

---
+++ src/test/java/com/picpay/challenge_backend/ChallengeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.picpay.challenge_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
